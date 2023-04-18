c12b8056d9af2c164f744dc79b7de9a4891b597c
k.kubota <k.kubota@cybermissions.co.jp>
2023-04-18 11:45:44 +0900

    log出力をループさせる

--- logwriter.sh
+++ logwriter.sh
@@ -14,9 +14,9 @@ FILE="$DIR/log.txt"
 
 for ((i=1; i<=10; i++))
 do
-    TODAY=`data'+%Y-%m-%d %H:%M:%S`
+    TODAY=`date '+%Y-%m-%d %H:%M:%S'`
     echo "$TODAY Log Write $i!!!" >> $FILE
-    sleep 5
+    sleep 1
 done
 
 
